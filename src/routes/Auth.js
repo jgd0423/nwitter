dc1b2c506a7e7ffee0df6b589ab1cdd2c76b5bf3
Jeong Geumdam <61661198+jgd0423@users.noreply.github.com>
2021-02-22 11:42:55 +0900

    #2.1 Login Form part One

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,4 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Auth = () => <span>Auth</span>;
+const Auth = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const onChange = (event) => {
+    const {
+      target: { name, value },
+    } = event;
+    if (name === 'email') {
+      setEmail(value);
+    } else if (name === 'password') {
+      setPassword(value);
+    }
+  };
+  //   const [form, setForm] = useState({ email: '', password: '' });
+  //   const onChange = ({ target: { name, value } }) =>
+  //     setForm({ ...form, [name]: value });   // name에 대괄호는 computed property name
+  const onSubmit = (event) => {
+    event.preventDefault();
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          name='email'
+          type='text'
+          placeholder='Email'
+          required
+          value={email}
+          onChange={onChange}
+        />
+        <input
+          name='password'
+          type='password'
+          placeholder='Password'
+          required
+          value={password}
+          onChange={onChange}
+        />
+        <input type='submit' value='Log In' />
+      </form>
+      <div>
+        <button>Continue with Google</button>
+        <button>Continue with Github</button>
+      </div>
+    </div>
+  );
+};
+
 export default Auth;
