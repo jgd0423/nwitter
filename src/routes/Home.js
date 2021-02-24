f6609fc49115afa4ea14d84183c52ecec9172636
Jeong Geumdam <61661198+jgd0423@users.noreply.github.com>
2021-02-24 11:36:41 +0900

    #4.2 Uploading

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,6 +1,7 @@
-import Nweet from 'components/Nweet';
-import { dbService } from 'fbase';
 import React, { useEffect, useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import { dbService, storageService } from 'fbase';
+import Nweet from 'components/Nweet';
 
 const Home = ({ userObj }) => {
   const [nweet, setNweet] = useState('');
@@ -18,12 +19,15 @@ const Home = ({ userObj }) => {
   }, []);
   const onSubmit = async (event) => {
     event.preventDefault();
-    await dbService.collection('nweets').add({
-      text: nweet,
-      createdAt: Date.now(),
-      creatorId: userObj.uid,
-    });
-    setNweet('');
+    const fileRef = storageService.ref().child(`${userObj.uid}/${uuidv4()}`);
+    const response = await fileRef.putString(attachment, 'data_url');
+    console.log(response);
+    // await dbService.collection('nweets').add({
+    //   text: nweet,
+    //   createdAt: Date.now(),
+    //   creatorId: userObj.uid,
+    // });
+    // setNweet('');
   };
   const onChange = (event) => {
     const {
@@ -45,7 +49,11 @@ const Home = ({ userObj }) => {
     };
     reader.readAsDataURL(theFile);
   };
-  const onClearAttachment = () => setAttachment(null);
+  const onClearPhoto = () => {
+    setAttachment(null);
+    const imgInputTag = document.querySelector('#imgInput');
+    imgInputTag.value = null;
+  };
 
   return (
     <div>
@@ -57,12 +65,17 @@ const Home = ({ userObj }) => {
           placeholder="What's on your mind?"
           maxLength={120}
         />
-        <input type='file' accept='image/*' onChange={onFileChange} />
+        <input
+          id='imgInput'
+          type='file'
+          accept='image/*'
+          onChange={onFileChange}
+        />
         <input type='submit' value='Nweet' />
         {attachment && (
           <div>
             <img src={attachment} width='50px' height='50px' />
-            <button onClick={onClearAttachment}>Clear</button>
+            <button onClick={onClearPhoto}>Clear</button>
           </div>
         )}
       </form>
