74b34f7eec1f9340c2f8bcf80e8a38e0dcf5ae45
Jeong Geumdam <61661198+jgd0423@users.noreply.github.com>
2021-02-24 11:02:08 +0900

    #4.1 Preview Images part Two

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -5,6 +5,7 @@ import React, { useEffect, useState } from 'react';
 const Home = ({ userObj }) => {
   const [nweet, setNweet] = useState('');
   const [nweets, setNweets] = useState([]);
+  const [attachment, setAttachment] = useState();
 
   useEffect(() => {
     dbService.collection('nweets').onSnapshot((snapshot) => {
@@ -37,10 +38,14 @@ const Home = ({ userObj }) => {
     const theFile = files[0];
     const reader = new FileReader();
     reader.onloadend = (finishedEvent) => {
-      console.log(finishedEvent);
+      const {
+        currentTarget: { result },
+      } = finishedEvent;
+      setAttachment(result);
     };
     reader.readAsDataURL(theFile);
   };
+  const onClearAttachment = () => setAttachment(null);
 
   return (
     <div>
@@ -54,6 +59,12 @@ const Home = ({ userObj }) => {
         />
         <input type='file' accept='image/*' onChange={onFileChange} />
         <input type='submit' value='Nweet' />
+        {attachment && (
+          <div>
+            <img src={attachment} width='50px' height='50px' />
+            <button onClick={onClearAttachment}>Clear</button>
+          </div>
+        )}
       </form>
       <div>
         {nweets.map((nweet) => (
