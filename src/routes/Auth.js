5b8caa33b0dc6cb46689617f8b6bba55d2a831fc
Jeong Geumdam <61661198+jgd0423@users.noreply.github.com>
2021-02-26 09:35:09 +0900

    #6.0 Cleaning JS

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,45 +1,8 @@
+import AuthForm from 'components/AuthForm';
 import { authService, firebaseInstance } from 'fbase';
-import React, { useState } from 'react';
+import React from 'react';
 
 const Auth = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [newAccount, setNewAccount] = useState(true);
-  const [error, setError] = useState('');
-
-  const onChange = (event) => {
-    const {
-      target: { name, value },
-    } = event;
-    if (name === 'email') {
-      setEmail(value);
-    } else if (name === 'password') {
-      setPassword(value);
-    }
-  };
-  //   const [form, setForm] = useState({ email: '', password: '' });
-  //   const onChange = ({ target: { name, value } }) =>
-  //     setForm({ ...form, [name]: value });   // name에 대괄호는 computed property name
-  const onSubmit = async (event) => {
-    event.preventDefault();
-    try {
-      let data;
-      if (newAccount) {
-        data = await authService.createUserWithEmailAndPassword(
-          email,
-          password
-        );
-      } else {
-        data = await authService.signInWithEmailAndPassword(email, password);
-      }
-      console.log(data);
-    } catch (error) {
-      setError(error.message);
-    }
-  };
-
-  const toggleAccount = () => setNewAccount((prev) => !prev);
-
   const onSocialClick = async (event) => {
     const {
       target: { name },
@@ -50,39 +13,12 @@ const Auth = () => {
     } else if (name === 'github') {
       provider = new firebaseInstance.auth.GithubAuthProvider();
     }
-    const data = await authService.signInWithPopup(provider);
-    console.log(data);
+    await authService.signInWithPopup(provider);
   };
 
   return (
     <div>
-      <form onSubmit={onSubmit}>
-        <input
-          name='email'
-          type='email'
-          placeholder='Email'
-          required
-          value={email}
-          onChange={onChange}
-        />
-        <input
-          name='password'
-          type='password'
-          placeholder='Password'
-          required
-          value={password}
-          onChange={onChange}
-        />
-        <input
-          type='submit'
-          value={newAccount ? 'Create Account' : 'Sign In'}
-        />
-        <br />
-        {error}
-      </form>
-      <span onClick={toggleAccount}>
-        {newAccount ? 'Sign In' : 'Create Account'}
-      </span>
+      <AuthForm />
       <div>
         <button onClick={onSocialClick} name='google'>
           Continue with Google
