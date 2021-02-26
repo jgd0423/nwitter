956b0648664084455ca24efaac647cabb60ab487
Jeong Geumdam <61661198+jgd0423@users.noreply.github.com>
2021-02-26 14:53:14 +0900

    #6.1 Styles

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -8,19 +8,22 @@ const Home = ({ userObj }) => {
   const [nweets, setNweets] = useState([]);
 
   useEffect(() => {
-    dbService.collection('nweets').onSnapshot((snapshot) => {
-      const nweetArray = snapshot.docs.map((doc) => ({
-        id: doc.id,
-        ...doc.data(),
-      }));
-      setNweets(nweetArray);
-    });
+    dbService
+      .collection('nweets')
+      .orderBy('createdAt', 'desc')
+      .onSnapshot((snapshot) => {
+        const nweetArray = snapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        setNweets(nweetArray);
+      });
   }, []);
 
   return (
-    <div>
+    <div className='container'>
       <NweetFactory userObj={userObj} />
-      <div>
+      <div style={{ marginTop: 30 }}>
         {nweets.map((nweet) => (
           <Nweet
             key={nweet.id}
